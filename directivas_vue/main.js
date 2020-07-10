e7f7ea35dbae5b875743a8a0c160b8c318ef33bd
pablofsj <52142556+pablofsj@users.noreply.github.com>
2020-07-09 23:13:20 -0400

    Subida de ejercicio directivas Vue

---
+++ directivas_vue/main.js
@@ -0,0 +1,10 @@
+let ejVue = new Vue ({
+    el: "#contenedor",
+    data : {
+        usuario : "Pablo", 
+        encabezado1: true,
+        miembros: ['Bart','Lisa','Homero','Marge','Maggie'],
+        comidas: ['Papas fritas','Pizza','Tocomple','Hamburguesa','Churrasco'],
+    }
+    
+})
